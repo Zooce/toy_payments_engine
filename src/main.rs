b97185def98d77a670ca44429cd18177b0e12687
Zooce <zooce@hey.com>
2022-09-10 12:13:17 -0700

    implement withdraws

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ impl Engine {
         // TODO: validate transaction (ignore if missing tx ID and non-disputed tx ID)
         match tx.tx_type {
             TxType::Deposit => acct.deposit(tx.amount.expect("deposit transactions must have an amount")),
+            TxType::Withdraw => acct.withdraw(tx.amount.expect("withdraw transactions must have an amount")),
             _ => todo!(),
         }
         _ = self.tx_map.entry(tx.tx_id).or_insert(tx); // TODO: dispute-related transactions do not get stored here, rather they modify transactions
@@ -67,6 +68,11 @@ impl Acct {
         self.total += amt;
         self.available += amt;
     }
+
+    fn withdraw(&mut self, amt: f64) {
+        self.total -= amt;
+        self.available -= amt;
+    }
 }
 
 #[cfg(test)]
@@ -74,22 +80,22 @@ mod test {
     use super::*;
     use csv::{Reader, ReaderBuilder, Trim};
 
-    const DEPOSITS: &str =
-        "type, client, tx, amount
-        deposit,    1,  1,  1.0
-        deposit,    2,  2,  2.0
-        deposit,    1,  3,  2.0";
-
     fn reader(data: &'static [u8]) -> Reader<&'static [u8]> {
         ReaderBuilder::new().trim(Trim::All).from_reader(data)
     }
 
     #[test]
     fn deposits() {
+        const DATA: &str =
+            "type, client, tx, amount
+            deposit,    1,  1,  1.0
+            deposit,    2,  2,  2.0
+            deposit,    1,  3,  2.0";
+
         let mut engine = Engine::default();
-        for res in reader(DEPOSITS.as_bytes()).deserialize() {
+        for res in reader(DATA.as_bytes()).deserialize() {
             let tx: Tx = res.expect("unable to deserialize row");
-            engine.process_tx(tx).expect("filed to process transaction");  // <--- the function under test
+            engine.process_tx(tx).expect("failed to process transaction");
         }
 
         // counts
@@ -103,6 +109,39 @@ mod test {
         // account funds
         assert_eq!(3.0, a1.available);
         assert_eq!(3.0, a1.total);
+        assert_eq!(0.0, a1.held);
+        assert!(!a1.locked);
+        assert_eq!(2.0, a2.available);
+        assert_eq!(2.0, a2.total);
+        assert_eq!(0.0, a2.held);
+        assert!(!a2.locked);
+    }
+
+    #[test]
+    fn withdraws() {
+        const DATA: &str =
+            "type, client, tx, amount
+            deposit,    1,  1,  1.0
+            deposit,    2,  2,  2.0
+            withdraw,   1,  3,  0.5";
+
+        let mut engine = Engine::default();
+        for res in reader(DATA.as_bytes()).deserialize() {
+            let tx: Tx = res.expect("unable to deserialize row");
+            engine.process_tx(tx).expect("failed to process transaction");
+        }
+
+        // counts
+        assert_eq!(3, engine.tx_map.len());
+        assert_eq!(2, engine.acct_map.len());
+
+        // account ids
+        let a1 = engine.acct_map.get(&1).expect("expected account for client {1}");
+        let a2 = engine.acct_map.get(&2).expect("expected account for client {2}");
+
+        // account funds
+        assert_eq!(0.5, a1.available);
+        assert_eq!(0.5, a1.total);
         assert_eq!(0.0, a1.held);
         assert!(!a1.locked);
         assert_eq!(2.0, a2.available);
