9544b747b93ca8a8021b6ff908163b6d57776e4b
Zooce <zooce@hey.com>
2022-09-10 07:29:05 -0700

    test for new accounts added for new clients
    I'm now at a point where I can start moving this temp logic from `main`
    into some real tests. I'm starting with verifying that client IDs we
    have not seen before get a new account added.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
+use std::collections::BTreeMap;
 use std::error::Error;
-use csv::Reader;
+// use csv::{Reader, ReaderBuilder, Trim};
 use serde::Deserialize;
 
+type AcctMap = BTreeMap<u16, Acct>;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let txs = "type,client,tx,amount\n\
-        deposit,1,1,1.0\n\
-        deposit,2,2,2.0\n\
-        deposit,1,3,2.0\n\
-        withdraw,1,4,1.5\n\
-        withdraw,2,5,3.0\n\
-        dispute,1,3,\n\
-        dispute,1,4,\n\
-        resolve,1,3,\n\
-        chargeback,1,4,";
-    // TODO: open file from first argument
-    let mut reader = Reader::from_reader(txs.as_bytes());
-    for result in reader.deserialize() {
-        let tx: Tx = result?;
-        println!("{tx:?}");
-        // TODO: process record/transaction
-    }
-    // TODO: write all accounts to std::io::stdout()
     Ok(())
 }
 
@@ -43,4 +28,55 @@ struct Tx {
     #[serde(rename = "tx")]
     tx_id: u32,
     amount: Option<f64>,
+}
+
+fn process_tx(tx: &Tx, accts: &mut AcctMap) { // TODO: will probably return Result
+    let acct = accts.entry(tx.client_id).or_insert_with(Acct::default); // TODO: what if all tx's for a client are invalid?
+    // TODO: validate transaction (ignore if missing tx ID and non-disputed tx ID)
+    // TODO: match on tx.tx_type (process accordingly)
+}
+
+#[derive(Debug, Default)]
+struct Acct {
+    available: f64,
+    held: f64,
+    total: f64,
+    locked: bool,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use csv::{Reader, ReaderBuilder, Trim};
+
+    const DEPOSITS: &str =
+        "type, client, tx, amount
+        deposit,    1,  1,  1.0
+        deposit,    2,  2,  2.0
+        deposit,    1,  3,  2.0";
+
+    /// A wrapper so I don't have to keep writing this in every test
+    struct AcctMngr {
+        accts: AcctMap,
+        reader: Reader<&'static [u8]>,
+    }
+    impl AcctMngr {
+        fn new(data: &'static [u8]) -> Self {
+            Self {
+                accts: AcctMap::new(),
+                reader: ReaderBuilder::new().trim(Trim::All).from_reader(data),
+            }
+        }
+    }
+
+    #[test]
+    fn deposits() {
+        let mut mngr = AcctMngr::new(DEPOSITS.as_bytes());
+        for res in mngr.reader.deserialize() {
+            let tx: Tx = res.expect("unable to deserialize row");
+            process_tx(&tx, &mut mngr.accts);  // <--- the function under test
+        }
+        assert!(mngr.accts.get(&1).is_some());
+        assert!(mngr.accts.get(&2).is_some());
+    }
 }
